43efdf4f5386e56eb5c962236e536e438773643b
ivie-gif <emwiongbon_ivie@yahoo.com>
2024-12-02 13:42:12 +0100

    working with two files

---
+++ ProjectFiles/examples.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func runExamples() {
+	age := 30
+	height := 5.9
+	fmt.Printf("Age: %d, Height: %.1f\n", age, height) 
+}
---
+++ ProjectFiles/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+fmt.Println("Hello, Ivie!")
+fmt.Println("Today is : ", time.Now())
+
+runExamples()
+}
